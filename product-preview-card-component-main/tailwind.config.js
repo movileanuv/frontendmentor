6335b5dade726fcfa994528d8d1a3ffa0adfa685
Victor <victormovileanu@gmail.com>
2022-07-01 00:34:58 +0200

    desktop version

--- product-preview-card-component-main/tailwind.config.js
+++ product-preview-card-component-main/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-        extend: {},
+        fontFamily: {
+            montserrat: ['Montserrat', 'sans-serif'],
+            fraunces: ['Fraunces', 'serif']
+        },
+        extend: {
+            colors: {
+                darkCyan: 'hsl(158, 36%, 37%)',
+                cream: 'hsl(30, 38%, 92%)',
+                veryDarkBlue: 'hsl(212, 21%, 14%)',
+                darkGrayishBlue: 'hsl(228, 12%, 48%)',
+                white: 'hsl(0, 0%, 100%)'
+            }
+        }
     },
     plugins: [],
 }
