bfdeaaad58c30600dc38dceebf1ef5e8c236b3d6
Victor <victormovileanu@gmail.com>
2022-07-17 13:29:07 +0200

    final

---
+++ cookie-preferences-modal/readme.md
@@ -0,0 +1,5 @@
+# Cookie Preferences Modal
+
+Practice for frontend development.
+
+Design from https://uidesigndaily.com/posts/figma-cookie-preferences-modal-cookies-settings-day-1581
---
+++ cookie-preferences-modal/tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['./*.html'],
+  theme: {
+    extend: {
+      colors: {
+        blue1: '#F1F4FD',
+        blue2: '#D2D9EF',
+        darkBlue: '#131339',
+        blue3: '#FAFBFF',
+        dark: '#131339'
+      },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
