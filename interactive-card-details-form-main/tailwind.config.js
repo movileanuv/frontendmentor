e3b5fab44c7193837deb7d8eced9e83fbf3ecc4f
Victor <victormovileanu@gmail.com>
2022-09-30 12:39:56 +0200

    style form

--- interactive-card-details-form-main/tailwind.config.js
+++ interactive-card-details-form-main/tailwind.config.js
@@ -4,7 +4,13 @@ module.exports = {
     "./src/**/*.{html,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightGrayishViolet: 'hsl(270, 3%, 87%)',
+        darkGrayishViolet: 'hsl(279, 6%, 55%)',
+        veryDarkViolet: 'hsl(278, 68%, 11%)',
+      }
+    },
   },
   plugins: [],
 }
